378a8540fd2159ddc8ca5167b5d61b0c0f51d4d8
Abadat Hossain <abadat26@gmail.com>
2024-10-22 23:14:29 +0600

    navbar and footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 
 import { Outlet } from 'react-router-dom'
+import Footer from '../Shared/Footer/Footer'
+import Navbar from '../Shared/Navbar/Navbar'
 
 export default function Main() {
   return (
     <div>
+        <Navbar/>
         <Outlet></Outlet>
+        <Footer/>
     </div>
   )
 }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <Root />,
+      element: <Main />,
     //   errorElement: <ErrorPage />,
       children: [
         {
           path: "/",
-          element: <Main/>,
+          element: <Home/>,
         },
       ],
     },
   ]);
+
+  export default router;
