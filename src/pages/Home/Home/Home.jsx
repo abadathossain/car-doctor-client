dfc87916eb9cfa230ba1746da05f2cf05ee7f455
Abadat Hossain <abadat26@gmail.com>
2024-10-23 10:24:38 +0600

    services section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from '../About/About'
 import Banner from '../Banner/Banner'
+import Services from '../Services/Services'
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Banner/>
       <About/>
+      <Services/>
 
     </div>
   )
