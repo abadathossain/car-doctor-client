3867311b1fe0e9fbca291e19d5403064a1d182d9
Abadat Hossain <abadat26@gmail.com>
2024-10-24 11:29:16 +0600

    almost done but some confusion

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -6,9 +6,16 @@ export default function Services() {
     const [services, setServices]=useState([])
 
     useEffect(()=>{
-        axios.get('services.json')
+        axios.get('http://localhost:5000/services')
         .then(data=>setServices(data.data))
-    })
+       
+    },[])
+
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/services')
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+    // }, [])
 
   return (
     <div className="mt-4">
